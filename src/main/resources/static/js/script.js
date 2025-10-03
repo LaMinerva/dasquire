7db0e1597fb84841085c7d17de382014554841c4
LaMinerva <joachimonyedikagi@gmail.com>
2025-10-03 19:55:34 +0200

    30 minutes nothing done; have to understand how click JS works

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,15 @@
-const textarea = document.getElementById("stringa-denaro");
+const input = document.getElementById("stringa-denaro");
 const button = document.getElementById("calcola");
+const output = document.getElementById("cript-area-response")
 
+button.addEventListener("clickdashboard", () => {
+    let stringaDenaro = input.value;
+    let risultato = output.value;
+
+    const arrayOfDenari = stringaDenaro.split(";")
+
+    risultato = stringaDenaro[1]
+
+    document.getElementById("cript-area-response").innerText = `${risultato}`
+
+})
